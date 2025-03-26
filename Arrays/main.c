f3e94be02ed2162866514e4f2e6ba38738559b4f
Mariano Gonzalez <marianomgm93@gmail.com>
2025-03-26 14:19:15 -0300

    guia4-26-3

--- Arrays/main.c
+++ Arrays/main.c
@@ -3,6 +3,7 @@
 #include "pila.h"
 #define IN 1
 #define OUT 0
+
 const int DIMENSION=100;
 void addNum(int cantidad,int arreglo[],int* validos);
 void addNumFloat(int cantidad,float arreglo[],int* validos);
@@ -10,9 +11,18 @@ void showArray(int arreglo[],int validos);
 void showArrayFloat(float arreglo[],int validos);
 void inicArray(int arreglo[]);
 void inicArrayFloat(float arreglo[]);
-void menu(int arreglo[],int* validos,Pila* pila,float arregloFloat[],int* validosFloat);
+void menu(int arreglo[],int* validos,Pila* pila,float arregloFloat[], int* validosFloat,char arregloChar[],int* validosChar);
 void menuFloat(float arreglo[],int* validos);
+void menuChar(char arreglo[],int* validos);
 void arrayToPila(int arr[],int validos,Pila* pila);
+int buscarElemento(char arreglo[],char letra,int validos);
+void buscarElementoMenu(char arreglo[],int validos);
+void cargarArrayChar(char arreglo[],int* validos);
+void insertarCaracterOrdenado(char arreglo[], char letra,int* validos);
+void insertarCaracterOrdenadoMenu(char arreglo[],int* validos);
+char maximoCaracter(char arreglo[],int validos);
+int esCapicua(int arreglo[],int validos);
+void invertirArreglo(int arreglo[],int validos);
 int main()
 {
     int arregloInt[DIMENSION];
@@ -22,16 +32,17 @@ int main()
     inicpila(&pila);
     int validos=0;
     int validosFloat=0;
-    menu(arregloInt, &validos,&pila,arregloFloat,&validosFloat);
+    int validosChar=0;
+    menu(arregloInt, &validos,&pila,arregloFloat,&validosFloat,arregloChar,&validosChar);
     return 0;
 }
-void menu(int arreglo[],int* validos,Pila* pila,float arregloFloat[], int* validosFloat)
+void menu(int arreglo[],int* validos,Pila* pila,float arregloFloat[], int* validosFloat,char arregloChar[],int* validosChar)
 {
 
     int option, status=IN;
     while(status)
     {
-        printf("0\tSalir\n1\tMenu Enteros\n2\tMenu Float\n");
+        printf("0\tSalir\n1\tMenu Enteros\n2\tMenu Float\n3\tMenu char\n");
         scanf("%i",&option);
         switch(option)
         {
@@ -46,7 +57,11 @@ void menu(int arreglo[],int* validos,Pila* pila,float arregloFloat[], int* valid
         case 2:
             menuFloat(arregloFloat,validosFloat);
             break;
+        case 3:
+            menuChar(arregloChar,validosChar);
+            break;
         }
+
     }
 
 }
@@ -55,7 +70,8 @@ void menuInt(int arreglo[],int* validos,Pila* pila)
     int option,status=IN;
     while(status)
     {
-        printf("0\tSalir\n1\tCargar Arreglo\n2\tSumar arreglo\n3\tArreglo a pila\n4\tMostrar arreglo\n");
+        printf("0\tAtras\n1\tCargar Arreglo\n2\tSumar arreglo\n3\tArreglo a pila\n4\tMostrar arreglo\n5\tEs capicua?\n"
+               "6\tInvertir arreglo\n");
         scanf("%i",&option);
         switch(option)
         {
@@ -76,6 +92,12 @@ void menuInt(int arreglo[],int* validos,Pila* pila)
             break;
         case 4:
             showArray(arreglo,*validos);
+            break;
+        case 5:
+            esCapicua(arreglo,*validos);
+            break;
+        case 6:
+            invertirArreglo(arreglo,*validos);
             break;
         }
     }
@@ -194,7 +216,7 @@ void menuFloat(float arreglo[],int* validos)
     int option,status=IN;
     while(status)
     {
-        printf("0\tSalir\n1\tCargar Arreglo Float\n2\tMostrar arreglo Float\n");
+        printf("0\tAtras\n1\tCargar Arreglo Float\n2\tMostrar arreglo Float\n");
         scanf("%i",&option);
         switch(option)
         {
@@ -281,29 +303,221 @@ void showArrayFloat(float arreglo[],int validos)
         }
     }
 }
-/**
-*6.​ Realizar una función que indique si un elemento dado se encuentra en un arreglo de
-*caracteres.
-*/
-void menuChar(char arreglo[],int* validos){
+
+void menuChar(char arreglo[],int* validos)
+{
     int option, status=IN;
     while(status)
     {
-        printf("0\tSalir\n1\tCargar String\n2\tMostrar string\n");
+        printf("0\tAtras\n1\tCargar String\n2\tMostrar string\n3\tBuscar elemento\n4\t"
+               "Insertar caracter ordenado\n5\tObtener maximo\n");
         scanf("%i",&option);
         switch(option)
         {
         case 0:
-            printf("Saliendoo!");
+            printf("Saliendoo!\n");
             status=OUT;
             break;
         case 1:
-            menuInt(arreglo, validos,pila);
+            cargarArrayChar(arreglo,validos);
+            break;
+        case 2:
+            printf("String:\n%s\n",arreglo);
+            break;
+        case 3:
+            buscarElementoMenu(arreglo,*validos);
+            break;
+        case 4:
+            insertarCaracterOrdenadoMenu(arreglo,validos);
+            break;
+        case 5:
+            printf("El caracter mas grande del arreglo es: %c\n", maximoCaracter(arreglo,*validos));
+            break;
+        }
+
+
+    }
+}
+void cargarArrayChar(char arreglo[],int* validos)
+{
+    int option,cantidad,status=IN;
+    while(status)
+    {
+        printf("1\tCargar cadena(max 100c)\n0\tAtras\n");
+        scanf("%i",&option);
+        switch(option)
+        {
+        case 0:
+            status=OUT;
             break;
 
-        case 2:
-            menuFloat(arregloFloat,validosFloat);
+        case 1:
+            printf("Cuantos caracteres desea ingresar?\n");
+            scanf("%i",&cantidad);
+            addChar(cantidad,arreglo,validos);
             break;
         }
     }
+}
+void addChar(int cantidad,char arreglo[],int* validos)
+{
+    char letra;
+
+    for(int i=0; i<cantidad; i++)
+    {
+        printf("Ingrese el caracter a agregar en el lugar %i:\n",*validos+i);
+        scanf(" %c",&letra);
+        fflush(stdin);
+        arreglo[*validos+i]=letra;
+    }
+    *validos+=cantidad;
+}
+/**
+*6.​ Realizar una función que indique si un elemento dado se encuentra en un arreglo de
+*caracteres.
+*/
+void buscarElementoMenu(char arreglo[],int validos)
+{
+    int option,status=IN;
+    char letra;
+    while(status)
+    {
+        printf("1\tBuscar un caracter\n0\tAtras\n");
+        scanf("%i",&option);
+        switch(option)
+        {
+        case 0:
+            printf("Volviendo al menu anterior\n");
+            status=OUT;
+            break;
+        case 1:
+            printf("Ingrese el caracter a buscar en el arreglo:\n");
+            scanf(" %c",&letra);
+            switch(buscarElemento(arreglo,letra,validos))
+            {
+            case 0:
+                printf("No existe ese elemento en el arreglo\n");
+                break;
+            case 1:
+                printf("El elemento se encuentra en el arreglo\n");
+                break;
+            }
+
+            break;
+        }
+    }
+}
+/**
+*@Param Retorna 1(TRUE) y 0(FALSE) segun el caracter se encuentre en el arreglo.
+*/
+int buscarElemento(char arreglo[],char letra,int validos)
+{
+    int condition=0;
+    for(int i=0; i<validos; i++)
+    {
+        if(arreglo[i]==letra)
+        {
+            condition=1;
+        }
+    }
+    return condition;
+}
+/**
+*7. Realizar una función que inserte un carácter en un arreglo ordenado alfabéticamente,
+*conservando el orden.
+*/
+void insertarCaracterOrdenadoMenu(char arreglo[],int* validos)
+{
+    char letra;
+    printf("ingrese el caracter que desea insertar\n");
+    fflush(stdin);
+    scanf(" %c",&letra);
+    insertarCaracterOrdenado(arreglo,letra,validos);
+}
+void insertarCaracterOrdenado(char arreglo[], char letra,int* validos)
+{
+    for(int i=(*validos)-1; i>=0; i--)
+    {
+        if(letra<arreglo[i])
+        {
+            arreglo[i+1]=arreglo[i];
+        }
+        else if(letra>arreglo[i]&&letra<arreglo[i+1])
+        {
+            arreglo[i+1]=letra;
+            printf("Caracter insertado!\n");
+        }
+
+    }
+    (*validos)++;
+}
+/**
+*8. Realizar una función que obtenga el máximo carácter de un arreglo dado.
+*/
+char maximoCaracter(char arreglo[],int validos)
+{
+    char maximo=arreglo[0];
+    for(int i=1; i<validos; i++)
+    {
+        if(arreglo[i]>maximo)
+        {
+            maximo=arreglo[i];
+        }
+    }
+
+    return maximo;
+}
+/**
+*9. Realizar una función que determine si un arreglo es capicúa
+*Devuelve 1 y 0 segun sea o no capicua.
+*/
+int esCapicua(int arreglo[],int validos)
+{
+    int capicua=1;
+    for(int i=0; i<validos/2; i++)
+    {
+        if(arreglo[i]!=arreglo[validos-1-i])
+        {
+            capicua=0;
+        }
+    }
+    if(capicua)
+    {
+        printf("Es capicua!\n");
+    }
+    else
+    {
+        printf("No es capicua =(\n");
+    }
+    return capicua;
+}
+/**
+*10. Realizar una función que invierta los elementos de un arreglo. (sin utilizar un arreglo auxiliar)
+*/
+void invertirArreglo(int arreglo[],int validos){
+    int num;
+    for(int i=0;i<validos/2;i++){
+        num=arreglo[validos-1-i];
+        arreglo[validos-1-i]=arreglo[i];
+        arreglo[i]=num;
+    }
+}
+/**
+*11. Ordenar un arreglo según los siguientes métodos:
+*a. Selección
+*b. Inserción
+*/
+void ordenamientoPorSeleccion(int arreglo[],int validos){
+    int min=arreglo[0];
+    for(int i=0;i<validos-1;i++){
+
+    }
+
+}
+int posicionMenor(int posicionInicial,int arreglo[],int validos){
+    int posicionDelMenor,menor=arreglo[posicionInicial];
+        for(int i=posicionInicial;i<validos;i++){
+
+        }
+
 }
--- Arrays/pila.c
+++ Arrays/pila.c
@@ -1,5 +1,6 @@
 #include "pila.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 void inicpila(P_Pila p)
 {
